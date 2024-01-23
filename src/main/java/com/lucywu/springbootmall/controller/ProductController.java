efc535bea27128ff1e985e1fc2d92270179a1c40
吳 蕥綺 <n0975116268@gmail.com>
2024-01-23 15:00:16 +0800

    Add Product Feature - Implement Update Product

--- src/main/java/com/lucywu/springbootmall/controller/ProductController.java
+++ src/main/java/com/lucywu/springbootmall/controller/ProductController.java
@@ -32,4 +32,19 @@ public class ProductController {
         Product product = productService.getProductById(productId);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+    @PutMapping("/products/{productId}")
+
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+        // check if product exists or not
+        Product product = productService.getProductById(productId);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        // update product
+        productService.updateProduct(productId,productRequest);
+        Product updateedProduct = productService.getProductById(productId);
+        return ResponseEntity.status(HttpStatus.OK).body(updateedProduct);
+
+    }
 }
