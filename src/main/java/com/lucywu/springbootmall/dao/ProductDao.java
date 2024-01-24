c30b528e240c417155f2dc34b4cbdda13dfbb92c
吳 蕥綺 <n0975116268@gmail.com>
2024-01-24 13:06:22 +0800

    Add Product Feature - Implement Get Product (Basic)

--- src/main/java/com/lucywu/springbootmall/dao/ProductDao.java
+++ src/main/java/com/lucywu/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.lucywu.springbootmall.dao;
 import com.lucywu.springbootmall.dto.ProductRequest;
 import com.lucywu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId,ProductRequest productRequest);
--- src/main/java/com/lucywu/springbootmall/service/ProductService.java
+++ src/main/java/com/lucywu/springbootmall/service/ProductService.java
@@ -3,10 +3,14 @@ package com.lucywu.springbootmall.service;
 import com.lucywu.springbootmall.dto.ProductRequest;
 import com.lucywu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId,ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
+
 }
